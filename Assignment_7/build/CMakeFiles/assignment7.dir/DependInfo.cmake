b196e0e44f50d359d7c5e751f1eaaaa35de05566
Lee Korff-Korn <leekk270@gmail.com>
2019-12-13 23:46:08 -0500

    completed assignment 7 sort of

---
+++ Assignment_7/build/CMakeFiles/assignment7.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/leekorff-korn/ucg/Assignment_7/src/helpers.cpp" "/Users/leekorff-korn/ucg/Assignment_7/build/CMakeFiles/assignment7.dir/src/helpers.cpp.o"
+  "/Users/leekorff-korn/ucg/Assignment_7/src/main.cpp" "/Users/leekorff-korn/ucg/Assignment_7/build/CMakeFiles/assignment7.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DATA_DIR=\"/Users/leekorff-korn/ucg/Assignment_7/data/\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../ext/eigen"
+  "/Users/leekorff-korn/ucg/ext/glfw/include"
+  "/Users/leekorff-korn/ucg/ext/glad/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/leekorff-korn/ucg/Assignment_7/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/leekorff-korn/ucg/Assignment_7/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment_7/build/CMakeFiles/assignment7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment7.dir/src/main.cpp.o"
+  "CMakeFiles/assignment7.dir/src/helpers.cpp.o"
+  "assignment7.pdb"
+  "assignment7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
